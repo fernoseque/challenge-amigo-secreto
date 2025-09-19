c2f139dcfa1571d23000853530721c2b8c18ef03
fernoseque <fernoseque@gmail.com>
2025-09-19 14:12:03 -0600

    Actualiza app.js

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaNombres = [];
+
+function agregarAmigo () {
+    let nombre = document.getElementById('amigo').value;
+
+    if (nombre === '') {
+        alert('Nombre inválido');
+    } else {
+        listaNombres.push(nombre);
+    }
+}
+
+function mostrarLista () {
+
+}
